17e7d3b261e775113ad0de288d5c4aad7603a21e
KarimovMurodilla <karimovmurodilla15@gmail.com>
2024-11-16 15:57:01 +0500

    bug fixes related with caching

--- src/api/products.py
+++ src/api/products.py
@@ -26,13 +26,14 @@ async def get_products(
     end = start + limit
 
     cache = Cache()
+    result = {}
 
     if not search:
-        print("Not search")
         data = await cache.redis_client.lrange("products", start, end-1)
         products = [json.loads(item) for item in data]
+        exists = await cache.exists("products")
 
-        if not products:
+        if not exists:
             products = await BillzService().get_products()
             await cache.set("count", len(products))
             for item in products:
@@ -40,13 +41,15 @@ async def get_products(
             await cache.redis_client.expire("products", 600)
             products = products[start:end]
 
-    else:
-        print("It is search")
+        count = await cache.get("count")
+        result["count"] = int(count)
 
+    else:
         data = await cache.redis_client.lrange("products", 0, -1)
         all_products = [json.loads(item) for item in data]
+        exists = await cache.exists("products")
 
-        if not all_products:
+        if not exists:
             products = await BillzService().get_products()
             await cache.set("count", len(products))
             for item in products:
@@ -54,10 +57,8 @@ async def get_products(
             await cache.redis_client.expire("products", 600)
 
         products = [item for item in all_products if search.lower() in item["name"].lower()]
+        result["count"] = len(products)
 
-    result = {}
-    count = await cache.get("count")
-    result["count"] = int(count)
     result["products"] = products
 
     return result
